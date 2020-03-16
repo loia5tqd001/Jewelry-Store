5ed9419e328193f73c6991f0359656310f95b2c8
Loi <loia5tqd001@gmail.com>
2020-03-16 10:04:42 +0700

    Remove redundant SliderArrow component

--- src/components/atoms/slider-arrow.component.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { ArrowContainer } from './slider-arrow.styles';
-
-function SliderArrow(props) {
-  return <ArrowContainer {...props} />;
-}
-
-export default SliderArrow;
